a95622fd50978eb0cc22ea9e022a93739f14b2e3
alebelcor <856838+alebelcor@users.noreply.github.com>
2018-01-24 17:55:14 -0700

    Add noisy to kill list

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -2,3 +2,5 @@
 
 # Get the movie draft results in the command-line.
 alias draft="movie-draft ~/projects/draft.json"
+
+# alias draft="~/projects/google-spreadsheet-reporter/cli.js ~/projects/draft.json ~/projects/google-spreadsheet-reporter/map.json"
