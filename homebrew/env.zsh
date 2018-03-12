a0bf8b60cce2bc6dcf2f6d7269b84e71add5bf4a
alebelcor <856838+alebelcor@users.noreply.github.com>
2018-03-12 11:30:15 -0700

    Add Homebrew and Homebrew Cask variables

---
+++ homebrew/env.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# Do not send analytics.
+export HOMEBREW_NO_ANALYTICS=1
+
+# Do not permit redirects from secure HTTPS to insecure HTTP.
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+# Abort installation of cask if not checksum is defined
+export HOMEBREW_CASK_OPTS="--require-sha"
