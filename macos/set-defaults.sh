f895ef725d09e815ef8cce855424680142dac25c
alebelcor <856838+alebelcor@users.noreply.github.com>
2018-10-01 11:22:46 -0700

    Add default settings for flash, popups and desktop notifications on Chrome

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -852,8 +852,17 @@ defaults write com.google.Chrome ReportUserIDData -bool false
 
 # -- Content settings
 
+# Ask before playing Flash content.
+defaults write com.google.Chrome DefaultPluginsSetting -int 3
+
+# Do not allow any site to show popups.
+defaults write com.google.Chrome DefaultPopupsSetting -int 2
+
 # Ask before accessing physical location.
 defaults write com.google.Chrome DefaultGeolocationSetting -int 3
+
+# Ask before showing desktop notifications.
+defaults write com.google.Chrome DefaultNotificationsSetting -int 3
 
 # -- Default search provider
 
