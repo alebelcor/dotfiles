b4b02b32788a336e0cf1fd564d927558f1ca51c8
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-02-02 15:55:58 -0700

    Add app installation check. Fix copy command

--- spectacle/install.sh
+++ spectacle/install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# Check for Spectacle.
+if ! test -d "/Applications/Spectacle.app"
+then
+	printf "\\r  [ \\033[0;33mWARN\\033[0m ] Spectacle.app is not installed.\\n"
+	exit 1
+fi
+
 printf "› Installing Spectacle keyboard shortcuts.\\n"
 
+# Get path to `spectacle` folder.
+DOTFILES_SPECTACLE="$(dirname $(realpath $0))"
+
 # Install Spectacle keyboard shortcuts.
-cp -r Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json 2> /dev/null
+cp "$DOTFILES_SPECTACLE/Shortcuts.json" "$HOME/Library/Application Support/Spectacle/Shortcuts.json"
