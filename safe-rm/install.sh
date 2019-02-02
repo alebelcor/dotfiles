dbadd2f8e5f608f4b4828f4461fcee1fe944ca35
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-02-02 16:21:19 -0700

    Fix ln command. Add documentation

--- safe-rm/install.sh
+++ safe-rm/install.sh
@@ -7,11 +7,11 @@ then
 	exit 1
 fi
 
-# Get the current directory path.
-DOTFILES_SAFE_RM="$(pwd -P)"
+# Get path to `safe-rm` folder.
+DOTFILES_SAFE_RM="$(dirname $(realpath $0))"
 
 # Create `.config` folder if it doesn't exist.
 mkdir -p "${HOME}/.config"
 
 # Create a symbolic link for the configuration file.
-ln -s "${DOTFILES_SAFE_RM}/safe-rm" "${HOME}/.config/safe-rm"
+ln -sf "${DOTFILES_SAFE_RM}/safe-rm" "${HOME}/.config/safe-rm"
