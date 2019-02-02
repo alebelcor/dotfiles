e33a7cadf83a0c425ebae5e4279f6ee0f0c142df
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-02-02 16:35:19 -0700

    Fix copy command

--- macos/set-user-agents.sh
+++ macos/set-user-agents.sh
@@ -7,18 +7,21 @@
 # Note: Set proper permissions for new `.plist`: `chmod 644 new.plist`
 #
 
+# Get path to `macos` folder.
+DOTFILES_MACOS="$(dirname $(realpath $0))"
+
 setup_agent() {
-	local destination="$HOME/Library/LaunchAgents/"
-	local configuration_file="$destination/$1"
+	local destination="${HOME}/Library/LaunchAgents/"
+	local configuration_file="${destination}/$1"
 
 	# Unload the configuration file, just in case.
-	launchctl unload "$configuration_file" > /dev/null 2>&1
+	launchctl unload "${configuration_file}" > /dev/null 2>&1
 
 	# Copy the configuration file.
-	cp --force "$1" "$destination"
+	cp "${DOTFILES_MACOS}/$1" "${destination}"
 
 	# Load the configuration file.
-	launchctl load "$configuration_file" > /dev/null
+	launchctl load "${configuration_file}" > /dev/null
 }
 
 printf "› Setting user agents.\\n"
