32ae3beeea2a2661d5f40313cc74bc05c9c022c0
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-06-30 13:23:23 -0700

    Update nodenv plugins installation to use Homebrew

--- node/install.sh
+++ node/install.sh
@@ -7,54 +7,44 @@ then
 	exit 1
 fi
 
-# Check for `git`.
-if test ! "$(command -v git)"
-then
-	printf "\\r  [ \\033[0;33mWARN\\033[0m ] git is not installed.\\n"
-	exit 1
-fi
-
 # Initialize `nodenv`.
 nodenv init > /dev/null 2>&1
-
-# Install `nodenv` plugins.
-mkdir -p "$(nodenv root)"/plugins
-cd "$(nodenv root)"/plugins || exit 1
-
-# Install the `nodenv-package-rehash` plugin.
-if test ! -d "nodenv-package-rehash"
-then
-	printf "› Installing nodenv-package-rehash plugin.\\n"
-	git clone https://github.com/nodenv/nodenv-package-rehash.git > /dev/null 2>&1
-
-	printf "› Initialize nodenv-package-rehash plugin.\\n"
-	nodenv package-hooks install --all > /dev/null 2>&1
-else
-	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-package-rehash is already installed.\\n"
-fi
 
 # Set path to current folder.
 DOTFILES_NODE="${DOTFILES}/node"
 
-# Install the `nodenv-default-packages` plugin.
-cp "${DOTFILES_NODE}/default-packages" "$(nodenv root)/"
-
-if test ! -d "nodenv-default-packages"
+# Setup default packages list.
+if test "$(brew list nodenv/nodenv/nodenv-default-packages)"
 then
-	printf "› Installing nodenv-default-packages plugin.\\n"
-	git clone https://github.com/nodenv/nodenv-default-packages.git > /dev/null 2>&1
+	# Set path to nodenv root.
+	NODENV_ROOT="$(nodenv root)"
+
+	# Ensure destination folder exists.
+	mkdir -p "${NODENV_ROOT}/plugins"
+
+	# Create a symbolic link for the file.
+	ln -sf "${DOTFILES_NODE}/default-packages" "${NODENV_ROOT}/default-packages"
 else
-	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-default-packages is already installed.\\n"
+	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-default-packages is not installed.\\n"
+	exit 1
 fi
 
-# Install the `nodenv-update` plugin.
-if test ! -d "nodenv-update"
+# Install the hooks for existing `nodenv` versions.
+if test "$(brew list nodenv/nodenv/nodenv-package-rehash)"
 then
-	printf "› Installing nodenv-update plugin.\\n"
-	git clone https://github.com/nodenv/nodenv-update.git > /dev/null 2>&1
+	nodenv package-hooks install --all > /dev/null 2>&1
+else
+	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-package-rehash is not installed.\\n"
+	exit 1
+fi
+
+# Update `nodenv` and plugins.
+if test "$(brew list nodenv/nodenv/nodenv-update)"
+then
 	nodenv update > /dev/null 2>&1
 else
-	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-update is already installed.\\n"
+	printf "\\r  [ \\033[00;34mINFO\\033[0m ] nodenv-update is not installed.\\n"
+	exit 1
 fi
 
 # Get the latest Node.js LTS version number.
@@ -66,13 +56,13 @@ LATEST_LTS_VERSION=$(curl -s https://nodejs.org/dist/index.json 2>/dev/null | \
 )
 
 # Check if that version is installed, otherwise install it.
-if ! (nodenv versions | grep -qe "$LATEST_LTS_VERSION")
+if ! (nodenv versions | grep -qe "${LATEST_LTS_VERSION}")
 then
 	printf "› Installing Node.js v%s.\\n" "${LATEST_LTS_VERSION}"
-	nodenv install "${LATEST_LTS_VERSION}" > /dev/null 2>&1
+	nodenv install "${LATEST_LTS_VERSION}" 1> /dev/null
 
 	printf "› Setting global version of Node for all shells to v%s.\\n" "${LATEST_LTS_VERSION}"
-	nodenv global "${LATEST_LTS_VERSION}" > /dev/null 2>&1
+	nodenv global "${LATEST_LTS_VERSION}" 1> /dev/null
 else
 	printf "\\r  [ \\033[00;34mINFO\\033[0m ] Node.js v%s is already installed.\\n" "${LATEST_LTS_VERSION}"
 fi
