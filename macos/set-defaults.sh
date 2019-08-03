b37c6f0e3d34012051ea56e7ec6bca1276bb331f
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-08-02 21:57:55 -0700

    Collapse tags on finder

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -413,6 +413,9 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # Disable the warning when changing a file extension.
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
+# Collapse "Tags"section on sidebar
+defaults write com.apple.finder SidebarTagsSctionDisclosedState -bool false
+
 # Enable spring loading for directories.
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
