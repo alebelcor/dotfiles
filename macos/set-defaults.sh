af50e33667a06f9a4b1a6a72ea5406a357920e72
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-08-02 21:37:14 -0700

    Remove unnecessary property

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -288,7 +288,7 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Use scroll gesture with the Ctrl (^) modifier key to zoom.
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
-defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+defaults write com.apple.universalaccess closeViewScrollWheelModifiersInt -int 262144
 # Follow the keyboard focus while zoomed in.
 defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
