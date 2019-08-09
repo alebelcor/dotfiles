71236777a87c31df8d4c6a5829b12ec9896147a1
alebelcor <856838+alebelcor@users.noreply.github.com>
2019-08-09 12:37:07 -0700

    Add setting to help with large repos

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,6 +3,9 @@
 # Prevent prompt from checking whether the current git remote has been updated.
 export PURE_GIT_PULL=0
 
+# Do not include untracked files in dirtiness check. Mostly useful on large repos.
+export PURE_GIT_UNTRACKED_DIRTY=0
+
 # Set up prompt.
 autoload -U promptinit
 promptinit
