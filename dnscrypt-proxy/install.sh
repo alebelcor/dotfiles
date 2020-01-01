70c44e3d9c9924abd25e8e8077c39e6ea499247d
alebelcor <856838+alebelcor@users.noreply.github.com>
2020-01-01 11:11:29 -0700

    Auto-reload BitBar plugin after installing it

--- dnscrypt-proxy/install.sh
+++ dnscrypt-proxy/install.sh
@@ -20,10 +20,10 @@ DNS_SERVERS="'cloudflare', 'google', 'quad9-dnscrypt-ip4-filter-pri'"
 # Add the servers to the configuration file.
 sed -i -re "s/^\#\s+server_names\s+=\s+.*/server_names = [${DNS_SERVERS}]/gi" "${HOMEBREW_PREFIX}/etc/dnscrypt-proxy.toml"
 
-# Create Bitbar plugin folder if it doesn't exist.
+# Create Bitbar plugins folder if it doesn't exist.
 mkdir -p "${HOME}/.config/bitbar"
 
-# Download DNSCrypt Switcher for Bitbar.
+# Download "dnscrypt-proxy switcher" plugin for Bitbar.
 cd "${HOME}/.config/bitbar"
 curl --location --remote-name --silent --header 'Accept: application/vnd.github.v3.raw' 'https://api.github.com/repos/jedisct1/bitbar-dnscrypt-proxy-switcher/contents/dnscrypt-proxy-switcher.10s.sh'
 
@@ -35,3 +35,6 @@ chmod +x "dnscrypt-proxy-switcher.10s.sh"
 
 # Start the service.
 sudo brew services start dnscrypt-proxy > /dev/null 2>&1
+
+# Reload the BitBar plugin, in case BitBar is already running.
+open 'bitbar://refreshPlugin?name=dnscrypt-proxy-switcher.*?.sh' > /dev/null 2>&1
