43e43b00d5080214c62d95407a5234d937a58c49
alebelcor <856838+alebelcor@users.noreply.github.com>
2020-04-19 14:53:25 -0700

    fix: Name and add URL

--- node/env.zsh
+++ node/env.zsh
@@ -3,5 +3,5 @@
 # Use sloppy mode by default, matching web browsers.
 export NODE_REPL_MODE='sloppy'
 
-# Opt-out of Scar analytics
+# Opt-out of Scarf (https://scarf.sh/) analytics
 export SCARF_ANALYTICS=false
