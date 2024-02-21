4e91e7a80ee418a1852ca746678a19472026a09a
alebelcor <856838+alebelcor@users.noreply.github.com>
2024-02-21 10:59:56 -0700

    reduce noise in npm

--- node/env.zsh
+++ node/env.zsh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
+export ADBLOCK=true
+
+# Disable Open Collective postinstall message
+# https://github.com/opencollective/opencollective-postinstall
+export DISABLE_OPENCOLLECTIVE=true
+
 # Use sloppy mode by default, matching web browsers.
 export NODE_REPL_MODE='sloppy'
+
+export NPM_CONFIG_FUND=false
 
 # Opt-out of Scarf (https://scarf.sh/) analytics
 export SCARF_ANALYTICS=false
