cf8154a1a39227e75f3d7a5156b633d8056e795f
alebelcor <856838+alebelcor@users.noreply.github.com>
2024-10-02 00:49:38 -0700

    remove auto update

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -3,6 +3,9 @@
 # Do not send analytics.
 export HOMEBREW_NO_ANALYTICS=1
 
+# Disable auto-update checks.
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 # Do not permit redirects from secure HTTPS to insecure HTTP.
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 
